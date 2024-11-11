affe9a98469a38bae2a212b53e34f58ce74ff40a
EnemySaga <89214182+IEnemyFiles@users.noreply.github.com>
2024-11-11 18:56:34 +0700

    Add files via upload

---
+++ lib/data/controller/refferal/refferal_screen_controller.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:xaxino/core/helper/shared_preference_helper.dart';
+import 'package:xaxino/data/model/global/response_model/response_model.dart';
+import 'package:xaxino/data/model/refferal/refferalModel.dart';
+import 'package:xaxino/data/repo/refferal/refferal_screen_repo.dart';
+import 'package:xaxino/view/components/snack_bar/show_custom_snackbar.dart';
+import 'package:get/get.dart';
+import 'package:xaxino/data/model/profile/profile_response_model.dart';
+
+class RefferalScreenController extends GetxController {
+  RefferalScreenRepo refferalScreenRepo;
+  ProfileResponseModel model = ProfileResponseModel();
+
+  RefferalScreenController({required this.refferalScreenRepo});
+
+  bool isLoading = false;
+  String username = "";
+
+  String savedUsername = "";
+  getUsername() {
+    savedUsername = refferalScreenRepo.apiClient.sharedPreferences.getString(SharedPreferenceHelper.userNameKey) ?? "";
+    update();
+  }
+
+  List<ReferralUsers> refferalData = [];
+
+  void allGamesInfo() async {
+    isLoading = true;
+    refferalData.clear();
+    getUsername();
+
+    update();
+    ResponseModel model = await refferalScreenRepo.refferalData();
+    if (model.statusCode == 200) {
+      RefferalDataModel refferalModel = RefferalDataModel.fromJson(jsonDecode(model.responseJson));
+      username = refferalModel.data?.referralUsers?.username ?? "";
+
+      refferalData.addAll(refferalModel.data?.referralUsers?.allReferrals ?? []);
+      update();
+    } else {
+      CustomSnackBar.error(errorList: [model.message]);
+    }
+    isLoading = false;
+    update();
+  }
+}
